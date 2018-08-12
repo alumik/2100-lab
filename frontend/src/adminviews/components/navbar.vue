552082cc693f2083c41a7a3b4891f35be876a4a9
JingmengLiu <ljmnankai@163.com>
2018-08-12 22:09:28 +0800

    Add navbars to user system and admin system. Ref #11

---
+++ frontend/src/adminviews/components/navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-navbar
+    toggleable="md"
+    type="dark"
+    variant="info"
+    class="navbar">
+
+    <b-navbar-toggle target="nav_collapse"/>
+
+    <b-navbar-brand
+      href="#"
+      disabled>2100实验室 管理后台
+    </b-navbar-brand>
+
+    <b-collapse
+      id="nav_collapse"
+      is-nav>
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-text>管理员一</b-nav-text>
+
+        <b-nav-item
+          href="#"
+          right>
+          注销
+        </b-nav-item>
+      </b-navbar-nav>
+
+    </b-collapse>
+  </b-navbar>
+
+</template>
+
+<script>
+export default {
+  name: 'AdminNavbar'
+}
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import AdminNavBar from '@/adminviews/components/navbar'
+import UserNavBar from '@/userviews/components/navbar'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'AdminNavBar',
+      component: AdminNavBar
+    },
+    {
+      path: '/user',
+      name: 'UserNavBar',
+      component: UserNavBar
     }
   ]
 })
