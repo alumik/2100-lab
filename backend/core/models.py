31aa009eb946f20f553f40e85fc77f6ee02e8d7b
Zhong Zhenyu <nczzy1997@gmail.com>
2018-08-14 02:27:09 +0800

    Remove redundant lines in core models. Ref #25

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.core.validators import RegexValidator
 from django.utils import timezone
 from django.db.models.query import QuerySet
 
