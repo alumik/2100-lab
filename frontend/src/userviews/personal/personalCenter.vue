93f85a078ae6ed1ef52f8da8971a8d6696717696
陈戌 <1612839@mail.nankai.edu.cn>
2018-08-13 14:12:22 +0800

    Remove left menu bar from some pages. Ref #66

---
+++ frontend/src/userviews/personal/personalCenter.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <b-navbar
+      toggleable="md"
+      type="dark"
+      variant="info"
+      class="navbar">
+
+      <b-navbar-toggle
+        target="nav_collapse"/>
+
+      <b-navbar-brand
+        href="#"
+        disabled>2100实验室</b-navbar-brand>
+
+      <b-collapse
+        id="nav_collapse"
+        is-nav>
+        <b-navbar-nav class="ml-auto">
+
+          <img
+            src="../../assets/logo.png"
+            class="img-rounded">
+
+          <b-nav-text>用户一</b-nav-text>
+
+          <b-nav-item
+            href="#"
+            right>
+            注销
+          </b-nav-item>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+    <b-card no-body>
+      <b-tabs
+        pills
+        card
+        vertical>
+        <b-tab
+          title="查看学习记录"
+          active>
+          Tab Contents 1
+        </b-tab>
+        <b-tab title="查看订单记录">
+          Tab Contents 2
+        </b-tab>
+      </b-tabs>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PersonalCenter'
+}
+</script>
+
+<style scoped>
+  img {
+    width: 40px;
+    height: 40px;
+  }
+</style>
