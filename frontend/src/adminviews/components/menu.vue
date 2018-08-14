705de18310048403ecb604fd75ca099fb663c295
陈戌 <1612839@mail.nankai.edu.cn>
2018-08-14 12:48:35 +0800

    Merge sidebar branch again. Ref #72

---
+++ frontend/src/adminviews/components/menu.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- Sidebar  -->
+  <nav id="sidebar">
+    <div class="sidebar-header">后台管理</div>
+
+    <ul class="components">
+      <li class="active">
+        <a href="#">课程管理</a>
+      </li>
+      <li>
+        <a href="#admin/message">留言管理</a>
+      </li>
+      <li>
+        <a href="#">用户管理</a>
+      </li>
+      <li>
+        <a href="#">订单管理</a>
+      </li>
+      <li>
+        <a href="#">管理员管理</a>
+      </li>
+      <li>
+        <a href="#">数据分析</a>
+      </li>
+      <li>
+        <a href="#">日志查询</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import './style/style.css'
+export default {
+  name: 'Menu'
+}
+</script>
+
+<style scoped>
+
+</style>
