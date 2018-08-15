928174f015fce0e8ddab8a18f433f39205bbecb1
Zhong Zhenyu <nczzy1997@gmail.com>
2018-08-15 21:26:45 +0800

    Implement course detail backend. Ref #47

--- backend/courses/views.py
+++ backend/courses/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Heroes, Course
+from customers.models import LearningLog
 
 
 def get_heroes(request):
@@ -80,3 +81,29 @@ def get_paid_course_list(request):
         },
         safe=False
     )
+
+
+def get_course_detail(request):
+    course_id = request.GET.get('id')
+    referer_id = request.GET.get('ref', '')
+    request.session['referer_id'] = referer_id
+    course = Course.objects.get(id=course_id)
+    course_detail = {
+        'id': course.id,
+        'thumbnail': str(course.thumbnail),
+        'title': course.title,
+        'description': course.description,
+        'price': course.price,
+        'up_votes': course.up_votes.count(),
+        'expire_duration': course.expire_duration,
+        'expire_time': None
+    }
+    if request.user.is_authenticated:
+        try:
+            learning_log = LearningLog.objects.get(course=course, customer=request.user)
+            expire_time = learning_log.expire_time
+            if expire_time is not None:
+                course_detail['expire_time'] = expire_time
+        except LearningLog.DoesNotExist:
+            pass
+    return JsonResponse(course_detail)
