3272837501754f4bdcabdff137bcbdd6c3ba4ce5
zhangxing <m15222276167@163.com>
2018-08-16 15:08:33 +0800

    Add burned course page. Ref #45

---
+++ frontend/src/userviews/burnedcoursepage/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    id="burnedcourse">
+    <div>
+      <UserNavbar/>
+    </div>
+    <div id="gif">
+      <img src="../../assets/404.gif">
+    </div>
+    <div>
+      <h4>Oops! 课程已焚毁</h4>
+      <h4>如有问题请联系客服：152222761676</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserNavbar from '../components/navbar'
+
+export default {
+  name: 'BurnedCourse',
+  components: {
+    UserNavbar
+  }
+}
+</script>
