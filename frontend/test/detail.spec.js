017a9863ddcaf2c1a05527f622e836b033392dc9
Zhong Zhenyu <nczzy1997@gmail.com>
2018-08-18 18:27:57 +0800

    Fix conflict between test and dev. Ref #136

--- frontend/test/detail.spec.js
+++ frontend/test/detail.spec.js
@@ -2,8 +2,8 @@ import { shallowMount } from '@vue/test-utils'
 import Detail from '@/adminviews/course/detail.vue'
 
 describe('add_course.vue', () => {
-    it('标题是"课程详情"', () => {
-        const wrapper = shallowMount(Detail);
-        expect(wrapper.find('h2').text()).toBe('课程详情');
-    })
+  it('标题是"课程详情"', () => {
+    const wrapper = shallowMount(Detail)
+    expect(wrapper.find('h2').text()).toBe('课程详情')
+  })
 })
