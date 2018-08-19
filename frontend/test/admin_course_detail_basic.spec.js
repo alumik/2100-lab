0e9e1d46cc973ccdcffe269d74392f1006c04a01
dingquan <tdingquan@gmail.com>
2018-08-19 18:26:10 +0800

    Correct test mistakes. Ref #152

--- frontend/test/admin_course_detail_basic.spec.js
+++ frontend/test/admin_course_detail_basic.spec.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
 
 import { shallowMount } from '@vue/test-utils'
-import Detail from '@/adminviews/course/detail.vue'
+import DetailBasic from '@/adminviews/course/components/detail_basic.vue'
 
 describe('课程详情页面单元测验', () => {
   it('标题是"课程详情"', () => {
-    const wrapper = shallowMount(Detail)
+    const wrapper = shallowMount(DetailBasic)
     expect(wrapper.find('h2').text()).toBe('课程详情')
   })
 })
