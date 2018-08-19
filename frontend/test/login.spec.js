584084ffcd57ab53346276b286c30c3746fa7653
陈戌 <1612839@mail.nankai.edu.cn>
2018-08-19 21:08:15 +0800

    Fix a bug about sending vertify code. Ref #147

---
+++ frontend/test/login.spec.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+
+import { shallowMount } from '@vue/test-utils'
+import PersonalCenter from '@/userviews/personal/personalCenter.vue'
+
+describe('个人中心单元测试', () => {
+  const wrapper = shallowMount(PersonalCenter)
+  const button = wrapper.find('#change')
+  it('点击个人中心的"修改"按钮后按钮文本变为"保存"', () => {
+    button.trigger('click')
+    expect(wrapper.vm.status).toEqual('保存')
+  })
+  it('昵称输入框文本可以编辑', () => {
+    expect(wrapper.vm.disabled).toEqual(false)
+  })
+  it('点击"保存"按钮后按钮文本变为"修改"', () => {
+    button.trigger('click')
+    expect(wrapper.vm.status).toBe('修改')
+  })
+})
