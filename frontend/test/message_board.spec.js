31a714904ecf3e2ac290469d57d0be95b28f6463
zhangxing <m15222276167@163.com>
2018-08-19 16:59:52 +0800

    Fix the wrong word in test page. Ref #153

--- frontend/test/message_board.spec.js
+++ frontend/test/message_board.spec.js
@@ -28,7 +28,7 @@ describe('留言板单元测试', () => {
     expect(wrapper.vm.message_list[0].num_of_praise).toBe(praiseNum + 1)
   })
 
-  it('点击“点踩”按钮点赞数加1', () => {
+  it('点击“点踩”按钮点踩数加1', () => {
     const detestNum = wrapper.vm.message_list[0].num_of_detest
     wrapper.findAll('#detest-button').at(0).trigger('click')
     expect(wrapper.vm.message_list[0].num_of_detest).toBe(detestNum + 1)
