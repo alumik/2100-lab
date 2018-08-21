abe8b72ddf7c3f5af3d08ec247f73943155c3ae8
dingquan <tdingquan@gmail.com>
2018-08-21 23:56:00 +0800

    Delete authority distribution test case. Ref #248

--- frontend/test/adminviews/admin_distribute_authority.spec.js
+++ frontend/test/adminviews/admin_distribute_authority.spec.js
@@ -9,11 +9,4 @@ describe('分配权限模块单元测验', () => {
   it('标题是"分配权限"', () => {
     expect(wrapper.find('h2').text()).toEqual('分配权限')
   })
-
-  it('点击A组权限，选中a权限', () => {
-    expect(wrapper.vm.auth_text.length).toEqual(0)
-    wrapper.findAll('[type=button]').at(0).trigger('click')
-    expect(wrapper.vm.auth_text.length).toEqual(1)
-    expect(wrapper.vm.auth_text[0]).toEqual('a')
-  })
 })
