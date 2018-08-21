8ae4edc033729d974f6b177615c1f445fa6efc11
dingquan <tdingquan@gmail.com>
2018-08-21 18:34:04 +0800

    Add all the missing changes. Ref #242

---
+++ backend/customers/utils.py
@@ -0,0 +1,21 @@
+from qcloudsms_py import SmsSingleSender
+from qcloudsms_py.httpclient import HTTPError
+
+def tencent_cloud_message(phone_number, verification_code):
+    appid = 1400131309  # SDK AppID是1400开头
+    appkey = "7901df2b3113f98f1a9ad266ca07c9db"
+    phone_numbers = phone_number
+    template_id = 178579
+    sms_sign = "南开软小依"
+    ssender = SmsSingleSender(appid, appkey)
+    params = [verification_code, '5']
+    try:
+        result = ssender.send_with_param(86, phone_numbers,
+                                         template_id, params, sign=sms_sign, extend="",
+                                         ext="")
+    except HTTPError as e:
+        print(e)
+    except Exception as e:
+        print(e)
+
+    print(result)
