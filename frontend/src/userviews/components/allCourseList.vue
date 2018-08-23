f8e8d49325655ad2d7a9c501e19e825f352cb04e
zhangxing <m15222276167@163.com>
2018-08-23 15:17:10 +0800

    Add connection to all course list page. Ref #285

--- frontend/src/userviews/components/allCourseList.vue
+++ frontend/src/userviews/components/allCourseList.vue
@@ -3,46 +3,39 @@
     <div
       id="page-title"
       class="title-style">
-      <h2>{{ page_title }}</h2>
+      <h5>{{ page_title }}</h5>
     </div>
     <div
       id="course-list"
       class="bv-example-row course-list-style">
-      <b-card-group
-        v-for="course in course_list"
-        :key="course.id"
-        deck
-        class="mb-3 b-card-group-style">
-        <b-card
-          :header="course.name"
-          border-variant="primary"
-          header-bg-variant="primary"
-          header-text-variant="white"
-          align="center">
-          <div class="card-content">
-            <div class="course-img-style">
-              <b-img
-                id="course-img"
-                :src="course.src"
-                fluid
-                thumbnail
-                alt="Responsive image Thumbnail"
-                class="img-thumbnail card-image"
-                @click="open_course_detail(course.id)"
-              />
-            </div>
-            <div class="introduction">
-              &emsp; &emsp;{{ course.introduction }}
-            </div>
-          </div>
-        </b-card>
-      </b-card-group>
+      <b-container class="bv-example-row">
+        <b-row>
+          <b-col
+            v-for="course in course_list"
+            :key="course.course_id"
+            class="col-style">
+            <b-card
+              id="course-img"
+              :img-src="example_src"
+              :title="course.title"
+              img-alt="Image"
+              img-top
+              tag="article"
+              class="mb-2 width-style"
+              @click="open_detail_page(course.course_id)">
+              <p class="card-text">
+                {{ course.description?course.description.substring(0,20):'' }}
+              </p>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-container>
     </div>
     <div class="pagination-style">
       <Pagination
         id="pagination"
         :rows="rows"
-        :perpage="perpage"
+        :perpage="page_limit"
         @change="change_page"/>
     </div>
   </div>
@@ -74,16 +67,19 @@ export default {
     page: {
       type: Number,
       default: 1
+    },
+    rows: {
+      type: Number,
+      default: 0
     }
   },
   data () {
     return {
-      rows: 100,
-      perpage: 15
+      example_src: 'https://picsum.photos/400/300/?image=32'
     }
   },
   methods: {
-    open_course_detail: function (id) {
+    open_detail_page: function (id) {
       this.$router.push({name: 'CourseDetail', params: {course_id: id}})
     },
     change_page: function (page) {
@@ -100,22 +96,6 @@ export default {
     text-align: left;
   }
 
-  .card-content {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-  }
-
-  .introduction {
-    width: 40%;
-    height: 100%;
-    margin-left: 10px;
-    text-align: left;
-  }
-
   #course-list {
     display: flex;
     flex-direction: row;
@@ -123,29 +103,22 @@ export default {
     justify-content: center;
   }
 
-  .card-image {
-    width: 100%;
-    height: 100%;
-    padding: 10px;
-    border: 2px solid #b8daff;
-  }
-
   .course-list-style {
     margin: 30px 0;
-  }
-
-  .b-card-group-style {
-    width: 30%;
-    margin: 10px;
-  }
-
-  .course-img-style {
-    width: 50%;
-    height: 100%;
   }
 
   .pagination-style {
     display: flex;
     justify-content: center;
+  }
+
+  .width-style {
+    min-width: 10rem;
+    max-width: 20rem;
+  }
+
+  .col-style {
+    flex: 1 0 33%;
+    margin-bottom: 20px;
   }
 </style>
