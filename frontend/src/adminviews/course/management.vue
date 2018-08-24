623b7a3e9718d50ad460db10fa6840fe33d4df20
dingquan <tdingquan@gmail.com>
2018-08-24 15:38:17 +0800

    Add the change_cover_picture function. Ref #265

--- frontend/src/adminviews/course/management.vue
+++ frontend/src/adminviews/course/management.vue
@@ -34,11 +34,105 @@
                   </h5>
                 </b-col>
               </b-row>
-              <b-row>
-                <UploadPicture
-                  upload_category="upload home cover pictures"/>
+              <b-row class="my-row">
+                <div class="my-row">
+                  <div
+                    v-for="image in originImageList"
+                    :key="image.hero_id"
+                    class="img-uploader-preview">
+                    <div class="preview-img">
+                      <b-img
+                        :src="image.image"
+                        thumbnail
+                        fluid
+                        alt="Thumbnail"/>
+                    </div>
+                    <img
+                      src="../../assets/logo.png"
+                      class="img-uploader-delete-btn"
+                      @click="deleteOriginImg(image.hero_id)">
+                  </div>
+                  <div
+                    ref="uploader"
+                    class="img-uploader"
+                    @drop="handleDrop">
+                    <p
+                      v-if="!hasImages"
+                      class="img-uploader-placeholder">{{ placeholder }}</p>
+                    <div
+                      v-if="hasImages"
+                      class="img-uploader-preview-list">
+                      <div
+                        v-for="(data,index) in imageDataList"
+                        :key="index"
+                        class="img-uploader-preview">
+
+                        <div class="preview-img">
+                          <b-img
+                            :src="data"
+                            thumbnail
+                            fluid
+                            alt="Thumbnail"/>
+                        </div>
+                        <div
+                          v-if="hasImages"
+                          class="img-uploader-mask">
+                          <p
+                            class="img-uploader-file-name"
+                            @click="openInput()">
+                            {{ placeholder }}</p>
+                        </div>
+                        <img
+                          src="../../assets/logo.png"
+                          class="img-uploader-delete-btn"
+                          @click="deleteImg(index)">
+                      </div>
+                    </div>
+                    <label
+                      v-if="!hasImages"
+                      for="inputID"
+                      class="img-uploader-label"/>
+                    <input
+                      id="inputID"
+                      ref="input"
+                      class="input-image col-lg-12"
+                      type="file"
+                      accept="image/gif,image/jpeg,image/jpg,image/png,image/svg"
+                      multiple="multiple"
+                      @change="handleFileChange">
+                  </div>
+                </div>
               </b-row>
+              <b-row align-v="start">
+                <b-col>
+                  <h5 class="text-left">
+                    输入文字
+                  </h5>
+                </b-col>
+              </b-row>
+              <div>
+                <textarea
+                  id="intro"
+                  v-model="input_content"
+                  class="form-control col-lg-12"
+                  rows="6s"/>
+              </div>
             </b-container>
+            <div
+              slot="modal-footer"
+              class="w-100">
+              <b-row class="define-btn">
+                <b-col cols="8"/>
+                <b-col cols="2">
+                  <b-button
+                    @click="uploadData">上传</b-button>
+                </b-col>
+                <b-col cols="2">
+                  <b-button
+                    @click="hideModal">取消</b-button>
+                </b-col>
+              </b-row>
+            </div>
           </b-modal>
         </div>
       </div>
@@ -62,7 +156,8 @@
                     class="form-control col-5"
                     placeholder=""
                     @keyup.enter="change">
-              </div></td>
+                </div>
+              </td>
               <td>
                 <div class="input-group my-long-input">
                   <input
@@ -111,9 +206,11 @@
 
 <script>
 import axios from 'axios'
+import qs from 'qs'
 import Basic from '../basic/basic'
 import UploadPicture from './upload_picture'
 import Pagination from '../../components/pagination'
+import resizeImage from './resize'
 export default {
   name: 'CourseManagement',
   components: {Pagination, UploadPicture, Basic},
@@ -132,7 +229,29 @@ export default {
       rows: 0,
       error_message: '',
       per_limit: 8,
-      page: 1
+      page: 1,
+
+      placeholder: '点击上传图片',
+      originImageList: [],
+      imageDataList: [],
+      fileNameList: [],
+      deleteOriginList: [],
+      input_content: ''
+    }
+  },
+  computed: {
+    hasImages () {
+      return this.imageDataList.length > 0
+    },
+    sizeFormatted () {
+      let result = 0
+      if (this.maxSize < 1024) {
+        result = this.maxSize + 'K'
+      } else {
+        result =
+          (this.maxSize / 1024).toFixed(this.maxSize % 1024 > 0 ? 2 : 0) + 'M'
+      }
+      return result
     }
   },
   created: function () {
@@ -148,7 +267,10 @@ export default {
         this.rows = response.data.count
         let _course = []
         for (let data of response.data.content) {
-          _course.push({'course_id': data['course_id'], 'codename': data['codename'], 'title': data['title'], 'updated_at': data['updated_at'].substring(0, 10)})
+          _course.push({'course_id': data['course_id'],
+            'codename': data['codename'],
+            'title': data['title'],
+            'updated_at': data['updated_at'].substring(0, 10)})
         }
         this.courses = _course
       }).catch(
@@ -162,6 +284,15 @@ export default {
       if (id === 0) {
         this.$router.push({name: 'AddCourse'})
       } else if (id === -1) {
+        axios.get('http://localhost:8000/api/v1/courses/forestage/main/get-heroes/').then(
+          response => {
+            this.originImageList = response.data.content
+            console.log(this.originImageList[0])
+            for (let i = 0; i < this.originImageList.length; i++) {
+              this.originImageList[i].image = this.$store.state.address.concat(this.originImageList[i].image)
+            }
+          }
+        )
         this.$refs.upload_picture.show()
       } else if (id > 0) {
         this.$router.push({name: 'BackendCourseDetail', query: {course_id: id - 1}})
@@ -199,6 +330,85 @@ export default {
     changePage: function (currentpage) {
       this.page = currentpage
       this.change()
+    },
+    preventDefaultEvent (eventName) {
+      document.addEventListener(
+        eventName,
+        function (e) {
+          e.preventDefault()
+        },
+        false
+      )
+    },
+    handleFileChange () {
+      let input = this.$refs.input
+      let files = input.files
+      this.preview(files)
+    },
+    handleDrop (e) {
+      let files = e.dataTransfer.files
+      this.preview(files)
+    },
+    openInput () {
+      this.$refs.input.click()
+    },
+    deleteImg (index) {
+      this.imageDataList.splice(index, 1)
+      this.fileNameList.splice(index, 1)
+    },
+    preview (files) {
+      let _this = this
+      if (!files || !window.FileReader) return
+
+      for (let i = 0; i < files.length; i++) {
+        let file = files[i]
+        let reader = new FileReader()
+        reader.onload = function (e) {
+          resizeImage(e.target.result, 150, 150, function (result) {
+            _this.imageDataList.push(result)
+            _this.fileNameList.push(file)
+          })
+        }
+        reader.readAsDataURL(file)
+      }
+    },
+    uploadData () {
+      let updateText = this.input_content.split('\n')
+      let formdata = new FormData()
+      for (let i = 0; i < this.fileNameList.length; i++) {
+        formdata.append('heroes', this.fileNameList[i])
+        formdata.append('captions', updateText[i])
+      }
+      axios.post('http://localhost:8000/api/v1/courses/backstage/course-management/add-hero/', formdata).then(
+        response => {
+          console.log(response.data)
+        }
+      ).catch(
+        error => {
+          console.log(error.response)
+        }
+      )
+      axios.post('http://localhost:8000/api/v1/courses/backstage/course-management/delete-hero/',
+        qs.stringify({
+          delete_list: this.deleteOriginList
+        }, {arrayFormat: 'repeat'})).then(
+        response => {
+          console.log(response.data)
+        }
+      )
+    },
+    deleteOriginImg (index) {
+      this.deleteOriginList.push(index)
+      let i = -1
+      for (i = 0; i < this.originImageList.length; i++) {
+        if (this.originImageList[i].hero_id === index) {
+          break
+        }
+      }
+      this.originImageList.splice(i, 1)
+    },
+    hideModal () {
+      this.$refs.upload_picture.hide()
     }
   }
 }
@@ -227,6 +437,12 @@ export default {
 
   .my-td {
     width: 250px;
+  }
+
+  .my-row {
+    width: 100%;
+    padding: 0;
+    margin: 0;
   }
 
   .my-in-for-btn {
@@ -279,5 +495,113 @@ export default {
   .btn:active,
   .in-btn:active {
     background-color: #5e0057;
+  }
+
+  .input-image {
+    display: none;
+  }
+
+  .img-uploader {
+    position: relative;
+    width: auto;
+    min-width: 260px;
+    max-width: 800px;
+    height: calc(150px + 25px * 2);
+    background: #ebebeb;
+    border-radius: 5px;
+  }
+
+  .img-uploader-placeholder {
+    position: absolute;
+    top: 50%;
+    width: 100%;
+    margin: 0;
+    font-size: 15px;
+    color: #aaa;
+    text-align: center;
+    transform: translate(0%, -50%);
+  }
+
+  .img-uploader-preview-list {
+    width: 100%;
+    height: calc(150px + 18px * 2);
+    overflow: hidden;
+    overflow-x: auto;
+    text-align: center;
+    white-space: nowrap;
+    -webkit-backface-visibility: hidden;
+    -webkit-overflow-scrolling: touch;
+  }
+
+  .img-uploader-preview {
+    z-index: 2;
+    display: inline-block;
+    min-height: 150px;
+    margin: 10px;
+    background: #333;
+    border-radius: 10px;
+    transition: 0.3s cubic-bezier(0.3, 0, 0.2, 1);
+  }
+
+  .img-uploader-mask {
+    position: absolute;
+    bottom: 0;
+    display: none;
+    width: 150px;
+    text-align: center;
+    background: rgba(0, 0, 0, 0.6);
+    border-radius: 1px;
+  }
+
+  .img-uploader-preview:hover {
+    transform: scale(1.02);
+  }
+
+  .img-uploader-preview:hover .img-uploader-mask {
+    display: block;
+  }
+
+  .img-uploader-delete-btn {
+    position: absolute;
+    top: 0;
+    right: 0;
+    display: none;
+    width: 25px;
+    height: 25px;
+    margin: 5px;
+  }
+
+  .img-uploader-preview:hover .img-uploader-delete-btn {
+    display: block;
+  }
+
+  .img-uploader-preview .preview-img {
+    width: 150px;
+    height: 150px;
+    overflow: hidden;
+  }
+
+  .img-uploader-label {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    margin-bottom: 0;
+    cursor: pointer;
+  }
+
+  .img-uploader-file-name {
+    display: inline-block;
+    max-width: 90%;
+    padding-top: 10px;
+    margin: 0;
+    overflow: hidden;
+    font-size: 5px;
+    color: white;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    cursor: pointer;
   }
 </style>
