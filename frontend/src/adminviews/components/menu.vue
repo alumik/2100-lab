593d3914c8005c3bd3af67e468ee0fb7adeb3ebb
陈戌 <1612839@mail.nankai.edu.cn>
2018-08-24 15:18:14 +0800

    Adjust the menu displaying toggler for admin system. Ref #304

--- frontend/src/adminviews/components/menu.vue
+++ frontend/src/adminviews/components/menu.vue
@@ -1,6 +1,8 @@
 <template>
   <!-- Sidebar  -->
-  <nav id="sidebar">
+  <nav
+    id="sidebar"
+    :class="{hide: hidden, 'sidebar': true}">
     <div class="sidebar-header">{{ $t('menu.header') }}</div>
     <ul class="components">
       <li
@@ -18,31 +20,75 @@ import './style/style.css'
 export default {
   name: 'Menu',
   props: {
+    hidden: {
+      type: Boolean,
+      default: true
+    },
     lists: {
       type: Array,
       default: () => [
-        {id: 1, text: this.$t('menu.course'), isActive: false, path: '/admin/course'},
-        {id: 2, text: this.$t('menu.message'), isActive: false, path: '/admin/message'},
-        {id: 3, text: this.$t('menu.user'), isActive: false, path: '/admin/user'},
-        {id: 4, text: this.$t('menu.order'), isActive: false, path: '/admin/order'},
-        {id: 5, text: this.$t('menu.admin'), isActive: false, path: '/admin/adminmanagement'},
-        {id: 6, text: this.$t('menu.data'), isActive: false, path: '/admin/data'},
-        {id: 7, text: this.$t('menu.log'), isActive: false, path: '/admin/log'}
+        {
+          id: 1,
+          text: this.$t('menu.course'),
+          isActive: false,
+          path: '/admin/course'
+        },
+        {
+          id: 2,
+          text: this.$t('menu.message'),
+          isActive: false,
+          path: '/admin/message'
+        },
+        {
+          id: 3,
+          text: this.$t('menu.user'),
+          isActive: false,
+          path: '/admin/user'
+        },
+        {
+          id: 4,
+          text: this.$t('menu.order'),
+          isActive: false,
+          path: '/admin/order'
+        },
+        {
+          id: 5,
+          text: this.$t('menu.admin'),
+          isActive: false,
+          path: '/admin/adminmanagement'
+        },
+        {
+          id: 6,
+          text: this.$t('menu.data'),
+          isActive: false,
+          path: '/admin/data'
+        },
+        {
+          id: 7,
+          text: this.$t('menu.log'),
+          isActive: false,
+          path: '/admin/log'
+        }
       ]
     }
   },
-  create () {
+  watch: {
+    hidden () {
+      console.log(123)
+    }
   },
+  create () {},
   methods: {
     jump: function (id) {
       this.$emit('jump', id)
-      this.$router.push({path: this.lists[id - 1].path})
-    }}
+      this.$router.push({ path: this.lists[id - 1].path })
+    }
+  }
 }
 </script>
 
 <style>
-  #sidebar {
-    position: fixed;
-  }
+#sidebar {
+  position: fixed;
+}
 </style>
