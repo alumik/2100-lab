4444e804a53f672f76a31e1ad73e017b2c1c4c77
JingmengLiu <ljmnankai@163.com>
2018-08-26 21:30:53 +0800

    Add unit test to the data module. Ref #354

---
+++ frontend/test/adminviews/time_data.spec.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-undef */
+
+import {shallowMount} from '@vue/test-utils'
+import TimeData from '@/adminviews/data/time_data'
+import Basic from '@/adminviews/basic/basic'
+import Alert from '@/components/alert'
+import BootstrapVue from 'bootstrap-vue'
+import Vue from 'vue'
+
+Vue.use(BootstrapVue)
+
+describe('数据分析页面2单元测试', () => {
+  const wrapper = shallowMount(TimeData)
+
+  it('数据分析页面2组件存在', () => {
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('包含Basic组件', () => {
+    expect(wrapper.contains(Basic)).toBe(true)
+  })
+
+  it('测试错误信息', () => {
+    expect(wrapper.find('#alert').isVisible()).toBe(true)
+  })
+
+  it('测试提示信息是否显示', () => {
+    expect(wrapper.find('.data-empty').isVisible()).toBe(true)
+  })
+
+  it('测试get_step函数', () => {
+    expect(wrapper.vm.get_step()).toBe(-1)
+    wrapper.vm.month = '1.1'
+    expect(wrapper.vm.get_step()).toBe(-2)
+  })
+})
