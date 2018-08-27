98ed60c9102a078ef609b3cc8d5fc5361a34d66e
dingquan <tdingquan@gmail.com>
2018-08-27 14:24:34 +0800

    Fix the bugs on text display of distribution authority page. Ref #377

--- frontend/test/adminviews/admin_distribute_authority.spec.js
+++ frontend/test/adminviews/admin_distribute_authority.spec.js
@@ -27,11 +27,11 @@ describe('分配权限模块单元测验', () => {
 
   it('转换信息测试1_1', () => {
     let s = wrapper.vm.transfer_permission('comment_admin')
-    expect(s).toEqual('评论管理权限')
+    expect(s).toEqual('留言管理权限')
   })
 
   it('转换信息测试2', () => {
-    let s = wrapper.vm.reverse_permission('评论管理权限')
+    let s = wrapper.vm.reverse_permission('留言管理权限')
     expect(s).toEqual('comment_admin')
   })
 
