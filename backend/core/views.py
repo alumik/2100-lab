3a07ec2fdcc36507c6e38e35301e77393f5cc8d4
Zhong Zhenyu <nczzy1997@gmail.com>
2018-08-29 20:13:32 +0800

    Backend Document - Core Module. Ref #398

--- backend/core/views.py
+++ backend/core/views.py
@@ -12,18 +12,37 @@ def is_authenticated(request):
     """判断用户是否已登录
 
     如果已登录返回ID、登录状态和是否为管理员
+
+    **示例URL**
+
+    .. code-block:: html
+
+        http://localhost/api/v1/core/auth/is-authenticated/
+
+    **返回值**
+
+    *成功：* HTTP200
+
+    .. code-block:: javascript
+
+        {
+            user_id: 1,             // 用户ID
+            is_authenticated: true, // 用户是否已登录
+            is_staff: true,         // 用户是否是管理员
+            admin_groups: [         // 用户权限组
+                'course_admin',
+                ...
+            ]
+        }
+
     """
 
     user = request.user
     admin_groups = []
     if user.is_staff:
-        try:
-            admin = get_user_model().objects.get(id=user.id, is_staff=True)
-        except get_user_model().DoesNotExist:
-            return JsonResponse({'message': ERROR['object_not_found']}, status=404)
-        for group in admin.groups.all():
+        for group in user.groups.all():
             admin_groups.append(group.name)
-        if admin.is_superuser:
+        if user.is_superuser:
             admin_groups.append('super_admin')
 
     return JsonResponse(
@@ -38,7 +57,25 @@ def is_authenticated(request):
 
 @login_required
 def logout(request):
-    """注销用户"""
+    """注销用户
+
+    **示例URL**
+
+    .. code-block:: html
+
+        http://localhost/api/v1/core/auth/logout/
+
+    **返回值**
+
+    *成功：* HTTP200
+
+    .. code-block:: javascript
+
+        {
+            message: 'User logged out.'
+        }
+
+    """
 
     auth.logout(request)
     return JsonResponse({'message': INFO['user_logged_out']})
